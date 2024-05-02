84e07af6cb7ed22ab2791267fcdf9a2cdb8f072c
Pooyan Ghanbari <93377835+PooyanGnb@users.noreply.github.com>
2024-05-02 13:57:28 +0330

    salary and experience filter functionality

--- app/Http/Controllers/PositionController.php
+++ app/Http/Controllers/PositionController.php
@@ -10,14 +10,23 @@ class PositionController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
         $positions = Position::query();
 
         $positions->when(request('search'), function($query) {
-            $query->where('title', 'like', '%'. request('search') . '%')
-            ->orWhere('description', 'LIKE', '%'. request('search') . '%');
+            $query->where(function($query) {
+                $query->where('title', 'like', '%'. request('search') . '%')
+                ->orWhere('description', 'LIKE', '%'. request('search') . '%');
+            });
+        })->when(request('min_salary'), function($query) {
+            $query->where('salary', '>=', request('min_salary'));
+        })->when(request('max_salary'), function($query) {
+            $query->where('salary', '<=', request('max_salary'));
+        })->when(request('experience'), function($query) {
+            $query->where('experience', request('experience'));
         });
+
 
         return view("position.index", ['positions' => $positions->get()]);
     }
