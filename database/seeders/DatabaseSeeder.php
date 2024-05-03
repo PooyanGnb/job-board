b14f6e32381f15cbadb247ff650b6100e2a50321
Pooyan Ghanbari <93377835+PooyanGnb@users.noreply.github.com>
2024-05-03 20:09:36 +0330

    employer factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Employer;
 use App\Models\Position;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -14,9 +15,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(300)->create();
+        $users = User::all()->shuffle();
 
-        Position::factory(100)->create();
+        for ($i = 0; $i < 20; $i++) {
+            Employer::factory()->create([
+                "user_id" => $users->pop()->id
+            ]);
+        }
+
+        $employers = Employer::all();
+
+        for ($i = 0; $i < 100; $i++) {
+            Position::factory()->create([
+                'employer_id' => $employers->random()->id
+            ]);
+        }
 
         // User::factory()->create([
         //     'name' => 'Test User',
