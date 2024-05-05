0f2c93f5ca95d8908a480268bb6be9a4e07add66
Pooyan Ghanbari <93377835+PooyanGnb@users.noreply.github.com>
2024-05-05 10:42:32 +0330

    refactor label

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -6,16 +6,14 @@
         <form action="{{ route('auth.store') }}" method="POST">
             @csrf
             <div class="mb-8">
-                <label for="email"
-                    class="mb-2 block text-sm font-medium text-slate-900">E-mail
-                </label>
+                <x-label for="email" :required="true">E-mail</x-label>
                 <x-text-input name="email" />
             </div>
 
             <div class="mb-8">
-                <label for="password" class="mb-2 block text-sm font-medium text-slate-900">
+                <x-label for="password" :required="true">
                     Password
-                </label>
+                </x-label>
                 <x-text-input name="password" type="password" />
             </div>
             <div class="mb-8 flex justify-between text-sm font-medium">
--- resources/views/position_application/create.blade.php
+++ resources/views/position_application/create.blade.php
@@ -16,14 +16,13 @@
     <form action="{{ route('positions.application.store', $position) }}" method="POST" enctype="multipart/form-data">
       @csrf
       <div class="mb-4">
-        <label for="expected_salary"
-          class="mb-2 block text-sm font-medium text-slate-900">Expected Salary</label>
+        <x-label for="expected_salary" :required="true">Expected Salary</x-label>
         <x-text-input type="number" name="expected_salary" />
       </div>
 
       <div class="mb-4">
-        <label class="mb-2 block text-sm font-medium text-slate-900">Upload CV</label>
-        <x-text-input type="file" name="cv" />
+      <x-label for="cv" :required="true">Upload CV</x-label>        
+      <x-text-input type="file" name="cv" />
       </div>
 
       <x-button class="w-full">Apply</x-button>
